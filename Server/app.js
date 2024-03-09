eb719c80feaeae633e576fa62668cd4be6fb949b
programmer-kabir <programmerkabirr@gmail.com>
2024-03-09 20:57:18 +0600

    mongo db connect successfully

--- Server/app.js
+++ Server/app.js
@@ -11,6 +11,36 @@ app.use(express.json());
 
 
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://shoeECommerce:fRxnxEwniPg1uwGH@cluster0.0i3pjbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("MONGODB Connect successfully😊😊😊");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get("/", (req, res) => {
   res.send("Sever is running 😊😊😊");
 });
