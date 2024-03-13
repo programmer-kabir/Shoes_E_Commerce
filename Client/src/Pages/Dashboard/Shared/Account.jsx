b6d0cef3ae4da93c9324b5a59312889d5b5438a4
programmer-kabir <programmerkabirr@gmail.com>
2024-03-13 17:09:27 +0600

    account time section

--- Client/src/Pages/Dashboard/Shared/Account.jsx
+++ Client/src/Pages/Dashboard/Shared/Account.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import useAuth from "../../../Components/Hooks/useAuth";
 
 const Account = () => {
-  return (
-    <div>Account</div>
-  )
-}
+  const { user } = useAuth();
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting;
 
-export default Account
+  if (currentHour >= 5 && currentHour < 12) {
+    greeting = "Good morning";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good night";
+  }
+  return (
+    <section className="px-5 pt-7">
+      <div>
+        <h1 className="text-base font-semibold">
+          {greeting}, {user?.displayName}!
+        </h1>
+        <p className="text-gray-700">
+          Here's what's happening with your store today.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Account;
